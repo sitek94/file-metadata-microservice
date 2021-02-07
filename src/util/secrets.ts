0ab8ba507366ed94ea3c0033fdafaf0d21560b27
sitek94 <msitkowski94@gmail.com>
2021-02-07 21:31:07 +0100

    remove mongodb/mongoose

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -7,21 +7,3 @@ if (fs.existsSync('.env')) {
 }
 
 export const ENVIRONMENT = process.env.NODE_ENV;
-const prod = ENVIRONMENT === 'production'; // Anything else is treated as 'dev'
-
-export const MONGODB_URI = prod
-  ? process.env['MONGODB_URI']
-  : process.env['MONGODB_URI_LOCAL'];
-
-if (!MONGODB_URI) {
-  if (prod) {
-    console.error(
-      '❌ No mongo connection string. Set MONGODB_URI environment variable.'
-    );
-  } else {
-    console.error(
-      '❌ No mongo connection string. Set MONGODB_URI_LOCAL environment variable.'
-    );
-  }
-  process.exit(1);
-}
